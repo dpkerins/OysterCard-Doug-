dea6b2d1ef6c67fe00924c5a2136c72d44a02010
Emmanuel Folorunsho <emmanuel@Emmanuels-MacBook-Air.local>
2021-09-27 16:12:24 +0100

    step 7,8,9,10
    Co-authored-by: Doug <douglaskerins@gmail.com>

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -3,10 +3,12 @@ class Oystercard
   attr_reader :limit
 
   DEFAULT_LIMIT = 90
+  MIN_BALANCE = 1
 
   def initialize(balance = 0, limit = DEFAULT_LIMIT)
     @balance = balance
     @limit = limit
+    @in_journey = false 
   end 
 
   def top_up(money)
@@ -14,10 +16,28 @@ class Oystercard
     @balance += money
   end
 
+  def in_journey? 
+    @in_journey
+  end
+
+  def touch_in
+    raise "Minimum balance for a single journey is £#{MIN_BALANCE}" if @balance < 1
+    @in_journey = true 
+  end 
+
+  def touch_out
+    deduct(MIN_BALANCE)
+    @in_journey = false 
+  end 
+
   private
 
   def exceed_limit(money)
     (@balance + money ) > @limit
+  end
+
+  def deduct(money)
+    @balance -= money
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -16,7 +16,42 @@ describe Oystercard do
       expect{subject.top_up(91)}.to(raise_error("You have reached your 90 pound limit"))
     end
   end
+=begin
+  describe ".deduct" do 
+    it "should allow user to have their fare deducted from their card" do 
+      subject.top_up(30)
+      subject.deduct(10)
+      expect(subject.balance).to(eq(20))
+    end
+  end
+=end 
+  describe ".in_journey?" do 
+    it "should return a boolean value for whether in_journey is true or false" do 
+      expect([true, false]).to(include(subject.in_journey?))
+    end
+  end
 
+  describe ".touch_in" do 
+    it "should set in_journey to true" do 
+      subject.top_up(20)
+      subject.touch_in
+      expect(subject.in_journey?).to(eq(true))
+    end
 
+    it "should raise an error if a users touches in with an insufficient balance" do 
+      expect{subject.touch_in}.to(raise_error("Minimum balance for a single journey is £1"))
+    end 
+  end
 
+  describe ".touch_out" do 
+    it "should set in_journey to false" do 
+      subject.touch_out
+      expect(subject.in_journey?).to(eq(false))
+    end
+
+    it "should reduce the balance by the minimum fare" do
+      subject.top_up(20)
+      expect{subject.touch_out}.to change{subject.balance}.by(-1)
+    end
+  end
 end 
