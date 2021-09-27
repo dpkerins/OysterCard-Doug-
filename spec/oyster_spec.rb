5191ae57f3478d34599864d37cf2c8fda42a5516
Emmanuel Folorunsho <emmanuel@Emmanuels-MacBook-Air.local>
2021-09-27 14:23:58 +0100

    first commit

---
+++ spec/oyster_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Oyster do
+  it "has a version number" do
+    expect(Oyster::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
